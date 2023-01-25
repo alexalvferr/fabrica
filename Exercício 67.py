d202cdc649eb828da13f8e6fea71489f7c17bf4d
Alexandre Alves Ferreira <64877348+alexalvferr@users.noreply.github.com>
2023-01-25 16:56:06 -0400

    Update Exercício 67.py

--- Exercício 67.py
+++ Exercício 67.py
@@ -1,12 +1,12 @@
 print("Digite seu Usuário e Senha: ")
 
-usuario=str(input("Usuário: "))
-senha=(input("Senha: "))
-while usuario==senha:
+login = str(input("Usuário: "))
+senha = (input("Senha: "))
+while login == senha:
 
 	print("ERRO:O USUÁRIO NÃO PODE SER IGUAL A SENHA, TENTE NOVAMENTE")
-	usuario=str(input("usuário--> "))
-	senha=str(input("senha-->"))
+	login = str(input("usuário: "))
+	senha = str(input("senha: "))
 
 else:
 	print("Cadastro Efetuado Com Sucesso")
